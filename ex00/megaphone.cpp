566e03737a15d423e624c7d1278182d29ba8525b
Neko-Sato <hisatosi35350@gmail.com>
2024-01-24 18:38:42 +0900

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 17:29:32 by hshimizu          #+#    #+#             */
+/*   Updated: 2024/01/24 18:31:08 by hshimizu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char *argv[])
+{
+	std::string s;
+
+	if (1 < argc)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			s = argv[i];
+			for (std::string::iterator c = s.begin(); c != s.end(); c++)
+				*c = std::toupper(*c);
+			std::cout << s;
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
+	return (0);
+}
